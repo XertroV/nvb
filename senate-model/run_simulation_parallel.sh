78e5f436f3003c0caa46e8b4cd062c35c9a5b611
Max Kaye <m@xk.io>
2015-04-28 23:27:29 +1000

    add years to trial for bulk testing

--- senate-model/run_simulation_parallel.sh
+++ senate-model/run_simulation_parallel.sh
@@ -1,20 +1,25 @@
-if [ $# -eq 0 ]
+if [ $# -lt 2 ]
   then
-    echo "Useage: run_simulation_parallel.sh TRIALS_N"
+    echo "Useage: run_simulation_parallel.sh TRIALS_N YEAR"
     exit
 fi
 
 N=$1
+year=$2
+halfpartic="--half-participation"
+#halfpartic=""
 
 echo "Starting NVB Simulation, $N trials per data-point"
 echo ""
+echo "Using as half participation flag: '$halfpartic'"
 
 function runmain {
     file=$1
     pc=$2
     state=$3
+    year=$4
     echo "Started: $state-$pc"
-    python3 -m dg main.dg --summary --loop $N --pc-yes $pc --nvb --half-participation $state-gvts.csv $state-first-prefs.csv >> $file
+    python3 -m dg main.dg --summary --loop $N --pc-yes $pc --nvb $halfpartic "$year/$state-gvts.csv" "$year/$state-first-prefs.csv" >> $file
     echo `cat $file | grep NVB | wc -l` ",$N,$state,$pc%" >> all.results
     echo "Finished: $state-$pc"
 }
@@ -24,9 +29,10 @@ rm all.results &>/dev/null
 for state in NSW VIC TAS WA SA QLD
   do for pc in 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95
     do
-    file="results$state$pc"
+    file="$year/results-$state-$pc.txt"
+    echo $file
     rm $file &>/dev/null
-    runmain $file $pc $state &
+    runmain $file $pc $state $year &
   done
 done
 
